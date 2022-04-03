65d298ef6463c0f3f449f508edb72136dde94c98
Isaac Donado <idonado@pop-os.localdomain>
2022-04-03 20:58:23 +0200

    Simple nestjs/nextjs app

---
+++ src/server/view/view.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ViewController } from './view.controller';
+import { ViewService } from './view.service';
+
+@Module({
+  controllers: [ViewController],
+  providers: [ViewService, ConfigService]
+})
+export class ViewModule {}
